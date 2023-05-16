ead3078b3fbb46c237ad1a1a4d0a1c52d4d5392b
TayRyzenQ <taypech447@gmail.com>
2023-05-16 12:24:21 -0500

    Primer repositorio de GitHub

---
+++ Objetos/Clases/Persona.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Objetos.Clases
+{
+
+    //la clase  es una plantilla que contiene objetos y metodos
+    public class Persona
+    {
+        //
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int Edad { get; set; }
+        public double Salario { get; set; }         // double se puso por le tamaño de la variable
+
+        public void Imprimir()
+        {
+            Console.WriteLine("Nombre: " + this.Nombre);        //el 'this' esta en un nombre que ya esta declarado, se puede ocupar en distintos metodos
+            Console.WriteLine("Apellido: " + this.Apellido);
+            Console.WriteLine("Edad:" + this.Edad);
+            Console.WriteLine("Salario: " + this.Salario);
+        }
+    }
+}
---
+++ Objetos/Program.cs
@@ -0,0 +1,44 @@
+﻿using Objetos.Clases;
+using System;
+
+namespace Objetos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //instanciar: crear algo real (ya sea un objeto o darle valores)
+            Persona persona = new Persona();            //persona es la variable para el objeto
+            persona.Nombre = "Juana";
+            persona.Apellido = "De la Cruz";
+            persona.Edad = 35;
+            persona.Salario = 10858.53;
+            persona.Imprimir();     //aqui mandamos a llamar al metodo 'Imprimir' que esta dentro de persona
+            Console.ForegroundColor= ConsoleColor.Cyan;
+            Console.WriteLine("==========================================================");
+            Console.ForegroundColor= ConsoleColor.White;
+
+            Persona persona2 = new Persona();
+            persona2.Nombre = "Leonarda";
+            persona2.Apellido = "Rojas";
+            persona2.Edad = 18;
+            persona2.Salario = 8058.53;
+            persona2.Imprimir();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("==========================================================");
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Persona persona3 = new Persona();
+            persona3.Nombre = "Chayanne";
+            persona3.Apellido = "Lopez";
+            persona3.Edad = 24;
+            persona3.Salario = 530.8;
+            persona3.Imprimir();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("==========================================================");
+            Console.ForegroundColor = ConsoleColor.White;
+
+
+        }
+    }
+}
